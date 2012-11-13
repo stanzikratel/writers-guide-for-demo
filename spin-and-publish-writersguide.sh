20387eabf84fffaf9d553af6bab7120fa30075fb
Diane Fleming <diane.fleming@rackspace.com>
2012-11-13 13:44:13 -0600

    Updates

--- spin-and-publish-writersguide.sh
+++ spin-and-publish-writersguide.sh
@@ -1,2 +1,2 @@
-mvn clean generate-sources && scp -r target/docbkx/webhelp/writers-guide docs-beta.rackspace.com:/var/www/writers/ 
+mvn clean generate-sources && scp -r target/docbkx/webhelp/writers-guide-external docs-beta.rackspace.com:/var/www/writers/ 
 # && scp -r target/docbkx/webhelp/bk-writers-guide-internal docs-beta.rackspace.com:/var/www/writers/
